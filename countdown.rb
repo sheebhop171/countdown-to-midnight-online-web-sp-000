b85da0d287aa76360382a8aab86ed80353770e53
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-16 14:45:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
-number = 10
-def countdown(number)
-  while number <= 10 do
-    puts '#{number} SECOND(S)!'
-    number -= 1
+n = 10
+def countdown(n)
+  while n <= 10 do
+    puts '#{n} SECOND(S)!'
+    n -= 1
     break
   end
     return "HAPPY NEW YEAR!"
